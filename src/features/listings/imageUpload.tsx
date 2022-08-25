70d7d11eac3d89b5c2d0edc0ea47e33a653f4443
madhukar123456 <shroudmg@gmail.com>
2022-08-25 12:00:50 +0545

    Add new features

---
+++ src/features/listings/imageUpload.tsx
@@ -0,0 +1,9 @@
+//import { Web3Storage } from "web3.storage";
+
+const uploadImage = async() => {
+    //const client = new Web3Storage({ token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDAwNmYxREM1NmNDZTUyOEQ2NERCMmYzQkI1NjJGNGI4NGFDMGU3ODYiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NjA5MDY1NzY4MDgsIm5hbWUiOiJsaWdvIn0.Eo-EfBo_5uiXZ416_C7QacOIEDhvGGcLX0Eh0IRQSlM" });
+    const fileInput = document.querySelector('input[type="file"]');
+    console.log(fileInput);
+}
+
+export {uploadImage};
