16a8d4ac758ce3d4edbac4eda00c074d727ea547
Madhukar Gaida <86058993+madhukar123456@users.noreply.github.com>
2022-09-19 10:33:08 +0545

    Add info on user settings

--- src/features/authentication/Auth.tsx
+++ src/features/authentication/Auth.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Web3Auth } from "@web3auth/web3auth";
 import { CHAIN_NAMESPACES, SafeEventEmitterProvider } from "@web3auth/base";
-// import RPC from "../../lib/web3RPC";
+import RPC from "../../lib/web3RPC";
 import "../../assets/css/App.css";
 // import { createCeramicDoc } from "../listings/createCeramicDoc";
 import ResponsiveAppBar from "../../pages/Navbar/Navbar";
@@ -31,6 +31,13 @@ function Auth() {
     null
   );
   const [ isLogged, setIsLogged ] = useState<boolean>(false);
+
+  // const [chainId, setChainId] = useState<any>(null);
+  // const [balance, setBalance] = useState<any>(null);
+  // const [address, setAccounts] = useState<any>(null);
+  // const [receipt, setTransaction] = useState<any>(null);
+  // const [signedMessage, setMessage] = useState<any>(null);
+  // const [privateKey, setPrivateKey] = useState<any>(null);
   useEffect(() => {
     const init = async () => {
       try {
@@ -69,35 +76,77 @@ function Auth() {
     console.log("TEST: logged in!");
   };
 
+
+  const getUserInfo = async () => {
+    if (!web3auth) {
+      console.log("web3auth not initialized yet");
+      return;
+    }
+    const user = await web3auth.getUserInfo();
+    return user;
+  };
+
   const logout = async () => {
     if (!web3auth) {
       console.log("web3auth not initialized yet");
       return;
     }
     await web3auth.logout();
-    setIsLogged(false);
     setProvider(null);
   };
 
-  // const getUserInfo = async () => {
-  //   if (!web3auth) {
-  //     console.log("web3auth not initialized yet");
-  //     return;
-  //   }
-  //   const user = await web3auth.getUserInfo();
-  //   console.log(user);
-  // };
+  const getChainId = async () => {
+    if (!provider) {
+      console.log("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    return await rpc.getChainId();
+  };
+  const getAccounts = async () => {
+    if (!provider) {
+      console.log("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    return await rpc.getAccounts();
+  };
 
-  // const getAccounts = async () => {
+  const getBalance = async () => {
+    if (!provider) {
+      console.log("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    return await rpc.getBalance();
+  };
+
+  // const sendTransaction = async () => {
   //   if (!provider) {
   //     console.log("provider not initialized yet");
   //     return;
   //   }
   //   const rpc = new RPC(provider);
-  //   const address = await rpc.getAccounts();
-  //   console.log(address);
-  //   return address;
+  //   return await rpc.sendTransaction();
   // };
+
+  // const signMessage = async () => {
+  //   if (!provider) {
+  //     console.log("provider not initialized yet");
+  //     return;
+  //   }
+  //   const rpc = new RPC(provider);
+  //   return await rpc.signMessage();
+  // };
+
+  const getPrivateKey = async () => {
+    if (!provider) {
+      console.log("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    return await rpc.getPrivateKey();
+  };
 
   if (provider) {
     return (
@@ -118,8 +167,8 @@ function Auth() {
               <Route path="bookmarks" element={<Bookmarks />} />
               <Route path="statistics" element={<Statistics />} />
               <Route path="messaging" element={<Messaging />} />
-              <Route path="settings" element={<UserSettings />} />
-            </Route>
+              <Route path="settings" element={<UserSettings getUserInfo={getUserInfo} getChainId={getChainId} getAccounts={getAccounts} getBalance={getBalance} getPrivateKey={getPrivateKey} />} />
+             </Route>
             <Route path="*" element={<PageNotFound />} />
           </Routes>
         </BrowserRouter>
